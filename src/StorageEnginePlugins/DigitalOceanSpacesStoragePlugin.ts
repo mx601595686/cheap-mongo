d8ebb2e5693333f3e587b1cebf3468b5e9a5e71d
wujingtao <601595686@qq.com>
2019-05-15 13:57:29 +0800

    优化了一下DigitalOcean Spaces Storage

--- src/StorageEnginePlugins/DigitalOceanSpacesStoragePlugin.ts
+++ src/StorageEnginePlugins/DigitalOceanSpacesStoragePlugin.ts
@@ -21,14 +21,14 @@ export = class DigitalOceanSpacesStoragePlugin implements BaseStorageEnginePlugi
     get name() { return 'spaces' }
 
     async getConnection(): Promise<BaseStorageEngineConnection> {
-        const pQueue = new PQueue({ interval: 1000, intervalCap: 200 });    //速度限制，按照digital ocean的要求最多每秒200个请求
-        const enableGzip = (process.env.ENABLE_GZIP || '').toLowerCase() === 'false' ? false : true;    //是否开启gzip压缩
+        const pQueue = new PQueue({ interval: 1000, intervalCap: 200 });    //速度限制。按照digital ocean的要求最多每秒200个请求
+        const enableGzip = (process.env.ENABLE_GZIP || '').toLowerCase() === 'false' ? false : true;    //是否开启gzip压缩，默认true
         const spaces = new aws.S3({
             endpoint: `https://${process.env.ENDPOINT}`,
             accessKeyId: process.env.ACCESS_KEY,
             secretAccessKey: process.env.SECRET,
             maxRetries: 5,
-            retryDelayOptions: { base: 1000 }
+            retryDelayOptions: { base: 100 }
         });
 
         const connection: BaseStorageEngineConnection = {
@@ -36,8 +36,8 @@ export = class DigitalOceanSpacesStoragePlugin implements BaseStorageEnginePlugi
                 return pQueue.onIdle();
             },
             async checkConnection() {
-                const result = await pQueue.add(() => spaces.getBucketVersioning({ Bucket: process.env.SPACE_NAME as string }).promise());
-                if (typeof result.Status !== 'string') throw new Error('DigitalOceanSpacesStoragePlugin 连接异常');
+                const result = await pQueue.add(() => spaces.getBucketLocation({ Bucket: process.env.SPACE_NAME as string }).promise());
+                if (typeof result.LocationConstraint !== 'string') throw new Error('DigitalOcean Spaces Storage 连接异常');
             },
             set(path: string, data: any) {
                 return pQueue.add(async () => {
@@ -52,15 +52,13 @@ export = class DigitalOceanSpacesStoragePlugin implements BaseStorageEnginePlugi
             },
             get(path: string) {
                 return pQueue.add(async () => {
-                    const result = await spaces.getObject({ Bucket: process.env.SPACE_NAME as string, Key: path }).promise();
+                    const result = await spaces.getObject({ Bucket: process.env.SPACE_NAME as string, Key: path }).promise()
+                        .catch((e: aws.AWSError) => { if (!e.message) e.message = e.code; throw e });
 
-                    if (result.Body) {
-                        if (result.ContentEncoding === 'gzip')
-                            return JSON.parse((await gunzip(result.Body as Buffer)).toString());
-                        else
-                            return JSON.parse(result.Body.toString());
-                    } else
-                        throw new Error(`没有找到对应的数据：'${path}'`);
+                    if (result.ContentEncoding === 'gzip')
+                        return JSON.parse((await gunzip(result.Body as Buffer)).toString());
+                    else
+                        return JSON.parse((result.Body as Buffer).toString());
                 }, { priority: 1 });
             },
             async delete(path: string) {
